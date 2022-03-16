d9ef8b6fc80099d63c01a1091180e10f65a20e7e
chacha-a-chacha <calex2607@gmail.com>
2022-03-16 15:57:19 +0000

    print_alphabet_x10 - prints out the alphabet  * main - uses built in function  * Return: 0 if succesful

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
 /**
+* print_alphabet_x10 - prints out the alphabet
+* main - uses built in function
+* Return: 0 if succesful
 * print_alphabet_x10
 * Print 10 times the alphabet, in lowercase.
 */
