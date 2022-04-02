5c14ce8c5d397eeccd31db7d37f9644597bc0ba9
chacha-a-chacha <calex2607@gmail.com>
2022-04-02 08:06:58 +0000

    betty linter check

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
 *@argv: an array containing the program command line arguments
 *Return: 0 if successful
 */
-int main(int argc,__attribute__((unused)) char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 printf("%d\n", argc - 1);
-return(0);
+return (0);
 }
