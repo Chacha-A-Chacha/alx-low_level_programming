0af84806e9282ba4f3c95adc7bc2195e9abd37e8
chacha-a-chacha <calex2607@gmail.com>
2022-04-02 08:44:10 +0000

    removal of whitespaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,12 @@
 
 /**
 *main - function that prints its name followed by a new line
-*@argc: the size of an argv array, the number of commandline arguments 
+*@argc: the size of an argv array, the number of commandline arguments
 *@argv: an array containing the program command line arguments
 *Return: 0 if succesful
 */
-
-int main(_attributre_((unused)) int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
-return(0);
+return (0);
 }
