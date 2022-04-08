4ddd550882d8ea0380e5ffd03b61f8a0f09ca2f5
chacha-a-chacha <calex2607@gmail.com>
2022-04-08 11:40:15 +0000

    Automatic and dynamic allocation, malloc and free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block using malloc and free (realloc)
+ * @ptr: pointer
+ * @num1_size: size of num1 pointer
+ * @num2_size: size of num2 pointer
+ * Return: new void pointer or Error if it fails
+ */
